bcec578a0940f14f180c9059757a19d6b2efb941
Sebastiano Guerzoni <seba@inbitcoin.it>
2021-04-25 07:26:09 +0200

    Add registry

--- coinaddress/networks/__init__.py
+++ coinaddress/networks/__init__.py
@@ -1,6 +1,7 @@
 from .registry import registry
 
 from .bitcoin import Bitcoin
+from .bitcoin_test import BitcoinTestnet
 from .bitcoin_cash import BitcoinCash
 from .ethereum import Ethereum
 from .litecoin import Litecoin
@@ -8,7 +9,7 @@ from .ripple import Ripple
 
 __all__ = [
     'registry',
-
+    'BitcoinTestnet',
     'Bitcoin',
     'BitcoinCash',
     'Ethereum',
